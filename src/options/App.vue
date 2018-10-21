61a05ea23650ab310249eeed6884389700ee4873
Wesley Chen <topolog@gmail.com>
2018-10-21 21:55:15 +0800

    Add option page

--- src/options/App.vue
+++ src/options/App.vue
@@ -1,17 +1,33 @@
 <template>
-  <div>
-    <p>Hello world!</p>
-  </div>
+  <OptionPanel :settings="settings" v-on:setting-change="onSettingChange"/>
 </template>
 
 <script>
+import OptionPanel from '../core/components/OptionPanel';
+import gSettingManager from '../core/model/setting';
+
 export default {
   name: 'App',
+
+  components: {
+    OptionPanel,
+  },
+  created: function() {
+    console.log('Created ImagePicker Options page');
+    // Load settings
+    this.settings = gSettingManager.loadSettings((loadedSetting, hasUpdate) => {});
+  },
+
+  data() {
+    return {
+      settings: null,
+    };
+  },
+
+  methods: {
+    onSettingChange: function(changedSettingName) {
+      gSettingManager.saveSettings();
+    },
+  },
 };
 </script>
-
-<style scoped>
-p {
-  font-size: 20px;
-}
-</style>
--- src/options/options.js
+++ src/options/options.js
@@ -1,5 +1,8 @@
 import Vue from 'vue';
 import App from './App';
+import '../core/plugins/vuetify';
+import 'roboto-fontface/css/roboto/roboto-fontface.css';
+import 'material-design-icons-iconfont/dist/material-design-icons.css';
 
 /* eslint-disable no-new */
 new Vue({
